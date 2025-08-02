3c6d777965440eb2158e0f86536baad9ec91adbd
Muhammad Guntur <mochtur@gmail.com>
2025-08-02 09:39:09 +0700

    sched/uclamp: Tune uclamp_assist
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8386,9 +8386,9 @@ static void uclamp_set(struct cgroup_subsys_state *css)
 
 	static struct uclamp_param tgts[] = {
 		{"top-app",             "0", "max",  1},
-       		{"foreground",          "0",  "80",  0},
-                {"dex2oat",             "0",  "30",  0},
-        	{"background",          "0",  "30",  0},
+       		{"foreground",          "0",  "max",  0},
+                {"dex2oat",             "0",  "60",  0},
+        	{"background",          "0",  "50",  0},
         	{"system-background",   "0",  "50",  0},
 	};
 
