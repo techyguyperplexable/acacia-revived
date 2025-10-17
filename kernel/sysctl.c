7ed884180cfe6d3ed0a47bbeb7d8f976d229f9df
skye-pa1n <sonicgames772@gmail.com>
2025-10-17 10:01:58 -0300

    sysctl: hardcode some variables

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1807,7 +1807,7 @@ static struct ctl_table vm_table[] = {
 		.procname	= "swappiness",
 		.data		= &vm_swappiness,
 		.maxlen		= sizeof(vm_swappiness),
-		.mode		= 0644,
+		.mode		= 0444,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
 #ifdef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
@@ -2026,7 +2026,7 @@ static struct ctl_table vm_table[] = {
 		.procname	= "vfs_cache_pressure",
 		.data		= &sysctl_vfs_cache_pressure,
 		.maxlen		= sizeof(sysctl_vfs_cache_pressure),
-		.mode		= 0644,
+		.mode		= 0444,
 		.proc_handler	= proc_dointvec,
 		.extra1		= &zero,
 	},
