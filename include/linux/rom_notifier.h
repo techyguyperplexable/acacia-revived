dbe569df31bf1558e4c4bc4284dd9825b1b3d11b
skye-pa1n <sonicgames772@gmail.com>
2025-12-21 12:40:35 -0300

    Revert "android: rom_notifier: micro-optimize"
    This reverts commit 8dec11c3494e803502be5ef1b15670a503171b17.

--- include/linux/rom_notifier.h
+++ include/linux/rom_notifier.h
@@ -3,7 +3,6 @@
  * Copyright (C) 2024 Samuel Pascua <pascua.samuel.14@gmail.com>.
  */
 
-#include <linux/cache.h>
 #include <linux/types.h>
 
-extern bool is_aosp __read_mostly;
+extern bool is_aosp;
