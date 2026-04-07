techyguyperplexable/acacia-revived

commit 93f667a55a62adac92aa566760aded9340f8b512

kernel/sys.c: Strip localversion from uname for GMS processes
Play Integrity now hashes the kernel's localversion and compares it to a
blacklist of localversion hashes for the purpose of failing Play Integrity
attestation.

Among the banned localversion hashes are "-Sultan" and "-sultan".

Since this is biased and doesn't improve security for end users in any way,
strip the localversion from uname for processes which are part of GMS's
thread group so that Play Integrity doesn't fail due to this.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
Signed-off-by: Josh Law <joshlaw48@gmail.com>

kernel/sys.c
@@ -1270,6 +1270,8 @@ extern void susfs_spoof_uname(struct new_utsname* tmp);
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
+	struct task_struct *t;
+	bool is_gms = false;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
@@ -1287,6 +1289,21 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	susfs_spoof_uname(&tmp);
 #endif
 	up_read(&uts_sem);
+
+	rcu_read_lock();
+	for_each_thread(current, t) {
+		if (thread_group_leader(t)) {
+			is_gms = !strcmp(t->comm, "id.gms.unstable");
+			break;
+		}
+	}
+	rcu_read_unlock();
+
+	if (is_gms)
+		snprintf(tmp.release, sizeof(tmp.release), "%u.%u.%u",
+			 LINUX_VERSION_MAJOR, LINUX_VERSION_PATCHLEVEL,
+			 LINUX_VERSION_SUBLEVEL);
+
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
 	if (override_release(name->release, sizeof(name->release)))

commit 6a0bbd5fd68d561723ff0d49bac6555cca8a071e

kernel: sys: use bitwise for LINUX_VERSION_*
The compiler freaks out using a undeclared identifier so this error is thrown

../kernel/sys.c:1304:5: error: use of undeclared identifier 'LINUX_VERSION_MAJOR'
 1304 |                          LINUX_VERSION_MAJOR, LINUX_VERSION_PATCHLEVEL,
      |                          ^
../kernel/sys.c:1304:26: error: use of undeclared identifier 'LINUX_VERSION_PATCHLEVEL'
 1304 |                          LINUX_VERSION_MAJOR, LINUX_VERSION_PATCHLEVEL,
      |                                               ^
../kernel/sys.c:1305:5: error: use of undeclared identifier 'LINUX_VERSION_SUBLEVEL'
 1305 |                          LINUX_VERSION_SUBLEVEL);
      |                          ^
3 errors generated.

Use bitwise shifts to mitigate this issue

Fixes: 93f667a55a62 ("kernel/sys.c: Strip localversion from uname for GMS processes")
Signed-off-by: Josh Law <joshlaw48@gmail.com>
Tested-by: Josh Law <joshlaw48@gmail.com>

kernel/sys.c
@@ -1301,8 +1301,8 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	if (is_gms)
 		snprintf(tmp.release, sizeof(tmp.release), "%u.%u.%u",
-			 LINUX_VERSION_MAJOR, LINUX_VERSION_PATCHLEVEL,
-			 LINUX_VERSION_SUBLEVEL);
+			 ((LINUX_VERSION_CODE >> 16) & 0xff), ((LINUX_VERSION_CODE >> 8) & 0xff),
+			 (LINUX_VERSION_CODE & 0xff));
 
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;